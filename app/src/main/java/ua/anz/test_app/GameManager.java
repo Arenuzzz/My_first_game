be7f904afefbf1e8947c4807e2a420396c79eca9
Arenuzzz <arenuzzz@mail.ru>
2015-12-17 20:21:42 +0200

    Урок 8 Добавили вражеские круги

--- app/src/main/java/ua/anz/test_app/GameManager.java
+++ app/src/main/java/ua/anz/test_app/GameManager.java
@@ -3,12 +3,16 @@ package ua.anz.test_app;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import java.util.ArrayList;
+
 /**
  * Created by AreNuZzz on 17.12.2015.
  */
 public class GameManager {
 
+    public static final int MAX_CIRCLES = 10;
     private MainCircle mainCircle;
+    private ArrayList<EnemyCircle> enemyCircles;
 
     private  static int width;
     private  static int height;
@@ -19,6 +23,16 @@ public class GameManager {
         width = w;
         height = h;
         initMainCircle();
+        initEnemyCircle();
+    }
+
+    private void initEnemyCircle() {
+       enemyCircles = new ArrayList<>();
+        for (int i = 0; i < MAX_CIRCLES; i++) {
+            EnemyCircle circle = EnemyCircle.getRandomCircle();
+            enemyCircles.add(circle);
+        }
+
     }
 
     public static int getWidth() {
@@ -37,6 +51,9 @@ public class GameManager {
 
     public void onDraw() {
         canvasView.drawCircle(mainCircle);
+        for (EnemyCircle circle : enemyCircles) {
+            canvasView.drawCircle(circle);
+        }
 
     }
 
